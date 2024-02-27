cf8e27ee373dfc87666be745a12487a9e694393f
HimsekharBhatta123 <137370695+HimsekharBhatta123@users.noreply.github.com>
2024-02-27 17:54:25 +0000

    created 01_variables.js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId=13225;
+let accountEmail="hb@gmail.com";
+var accountPassword="12345";
+accountCity="Guwahati"
+let accountState;
+// accountId=13255; //not allowed
+accountEmail="hbhb@gmail.com";
+accountPassword="1234";
+accountCity="Mumbai"
+
+// console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
