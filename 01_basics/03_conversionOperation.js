fb15a74e10e7aa024a0041e5eaffa86cc7dcc92c
HimsekharBhatta123 <137370695+HimsekharBhatta123@users.noreply.github.com>
2024-02-28 16:47:56 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,14 @@
+let score="33ana"
+console.log(typeof score)
+
+let valueInNumber=Number(score)
+console.log(valueInNumber);
+
+let isLogedIn=0
+let boolean=Boolean(isLogedIn)
+console.log(boolean);
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
