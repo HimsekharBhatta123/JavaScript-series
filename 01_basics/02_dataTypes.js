8d34eebd8f79d0f7d7e800f788bf559b87a565a6
HimsekharBhatta123 <137370695+HimsekharBhatta123@users.noreply.github.com>
2024-02-28 16:30:12 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,14 @@
+let name="Himsekhar"
+let age=22
+let isAdmin=false
+let state
+
+//number 2 to power 53
+//bigint
+//string=>""
+//boolean=>true/false
+//null=>stand alone value
+//undefined=>
+//symbol=>unique
+//object
+console.log(typeof isAdmin)
